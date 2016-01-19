182d15c3e9f901912af065765eb03a6e9bb07aea
linda90321 <linda90321@gmail.com>
2016-01-19 01:36:26 -0800

    Hash Table and String

---
+++ LeetCode_Java/src/group_Anagrams_49.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class group_Anagrams_49 {
+
+    public ArrayList<ArrayList<String>> groupAnagrams(String[] strs) {
+    	ArrayList<ArrayList<String>> res= new ArrayList<ArrayList<String>>();
+        if(strs.length == 0 || strs == null)
+        return res;
+        
+        Arrays.sort(strs);
+        HashMap<String, ArrayList<String>> map= new HashMap<String, ArrayList<String>>();
+        for(int i=0;i<strs.length;i++){
+            String[] sarr=strs[i].split("");
+            Arrays.sort(sarr);
+            String ss=Arrays.toString(sarr);
+            if(!map.containsKey(ss)){
+            ArrayList<String> temp= new ArrayList<String>();
+            temp.add(strs[i]);
+            map.put(ss,temp);
+            }else{
+                map.get(ss).add(strs[i]);
+            }
+        }
+        
+        for(ArrayList<String> arr : map.values() ){
+            if(arr != null)
+            res.add(arr);
+        }
+        
+        return res;
+    }
+
+}
