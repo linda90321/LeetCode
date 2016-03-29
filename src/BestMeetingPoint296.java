604cab30fd4085ca50e02e680e3578535839a3b5
linda90321 <linda90321@gmail.com>
2016-03-29 01:33:53 -0700

    leetcode java

---
+++ src/BestMeetingPoint296.java
@@ -0,0 +1,9 @@
+
+public class BestMeetingPoint296 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/numberOfIslands_II305.java
@@ -0,0 +1,4 @@
+//http://buttercola.blogspot.com/2016/01/leetcode-number-of-islands-ii.html
+public class numberOfIslands_II305 {
+
+}
